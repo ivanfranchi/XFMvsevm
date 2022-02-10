593b280614ee18826b3693b76207e2020c9bb6da
Ivan F. Clevo <iv20056@yahoo.it>
2022-02-10 13:57:02 +0000

    Add project files.

---
+++ XFMvsevm/XFMvsevm/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XFMvsevm.ViewModels;
+using XFMvsevm.Views;
+
+namespace XFMvsevm
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ XFMvsevm/XFMvsevm/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XFMvsevm.ViewModels;
+
+namespace XFMvsevm.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
